c8f07ba071e5bde4ee936d537f00eaaa33b3caf1
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-10-08 03:53:03 +0100

    Fix typo in relation with auth required of jwt

--- app/views.py
+++ app/views.py
@@ -9,22 +9,29 @@ from flask_jwt_extended import jwt_required
 from . import app, db
 from .models import AggregatedData
 
+
 @app.route("/uav/<id>")
-@jwt_required
+@jwt_required()
 def get_uav1(id):
     response = {}
 
-    uav_data = db.session.query(AggregatedData).filter(AggregatedData.topic.startswith(f"uav{id}/")).order_by(AggregatedData.timestamp.desc()).limit(11).all()
-    
-    if not uav_data :
-        return {"msg":"No such drone"}, 404
+    uav_data = (
+        db.session.query(AggregatedData)
+        .filter(AggregatedData.topic.startswith(f"uav{id}/"))
+        .order_by(AggregatedData.timestamp.desc())
+        .limit(11)
+        .all()
+    )
+
+    if not uav_data:
+        return {"msg": "No such drone"}, 404
     for el in AggregatedData.serialize_list(uav_data):
         response[el["topic"]] = el["value"]
 
     return response
 
 
-#Login and Auth routes
+# Login and Auth routes
 # Create a route to authenticate your users and return JWTs. The
 # create_access_token() function is used to actually generate the JWT.
 @app.route("/login", methods=["POST"])
@@ -35,11 +42,12 @@ def login():
         return jsonify({"msg": "Bad username or password"}), 401
 
     access_token = create_access_token(identity=username)
-    return jsonify(access_token=access_token)   
+    return jsonify(access_token=access_token)
 
-@app.route("/fire_notification",methods = ["POST"])
+
+@app.route("/fire_notification", methods=["POST"])
 def fire_notification():
-    #Do some logic 
-    
-    #send notification to react app
+    # Do some logic
+
+    # send notification to react app
     return "notification recieved"
