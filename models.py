33d4aab583a7067f5b9486348e87063e5bbdf8de
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2023-10-08 03:52:06 +0100

    Delete usless models file

--- models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from time.date import date
-
- # Define a model for the aggregated data
-class AggregatedData(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    topic = db.Column(db.String(255))
-    value = db.Column(db.String(255))
-    timestamp = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
